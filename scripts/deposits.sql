50deecef743970c7d3785a2dd152d4d8b01d0c07
Jeremy Tischer <jtischermba@gmail.com>
2026-04-02 19:49:38 -0400

    add deposit table

---
+++ scripts/deposits.sql
@@ -0,0 +1,16 @@
+-- scripts/create_table.sql
+USE ROLE github_cicd_role;
+USE WAREHOUSE COMPUTE_WH;
+USE DATABASE raw_prod;
+USE SCHEMA test;
+
+
+CREATE TABLE IF NOT EXISTS deposits (
+    id INT,
+    product STRING,
+    status STRING,
+    created_at TIMESTAMP_NTZ DEFAULT CURRENT_TIMESTAMP()
+);
+
+INSERT INTO deposits (id, product, status) 
+VALUES (1, 'young saver', 'open');
