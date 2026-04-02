2360fe188a1549b57d2accc0bb45c6b3f0ac26d7
Jeremy Tischer <jtischermba@gmail.com>
2026-04-02 16:37:38 -0400

    add loans file

---
+++ scripts/loans.sql
@@ -0,0 +1,15 @@
+-- scripts/create_table.sql
+USE ROLE github_cicd_role;
+USE WAREHOUSE COMPUTE_WH;
+USE DATABASE raw_prod;
+USE SCHEMA test;
+
+CREATE TABLE IF NOT EXISTS loans (
+    id INT,
+    product STRING,
+    status STRING,
+    created_at TIMESTAMP_NTZ DEFAULT CURRENT_TIMESTAMP()
+);
+
+INSERT INTO loans (id, product, status) 
+VALUES (1, 'Mortgage', 'Default');
